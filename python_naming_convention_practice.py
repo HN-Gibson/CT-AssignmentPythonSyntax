0c8b7b0c92e0929831240cd786885952b479bc71
H.N. Gibson <revhngibson@gmail.com>
2024-08-13 21:26:33 -0500

    completed python_naming_convention_practice.py

---
+++ python_naming_convention_practice.py
@@ -0,0 +1,10 @@
+#Objective: The aim of this assignment is to understand and apply Python's naming conventions, particularly for variables and constants.
+
+#Task 1: Code Correction: You have been given a piece of code with several variable and constant names that don't follow the Python naming convention. Your task is to correct them:
+
+PI = 3.14159 #changed to "PI" to shorten, used uppercase to note it is a constant, and added 3 digits to make calculations more accurate
+user_age = 25 #switched to snake_case to avoid case confusion in referencing the variable
+user_state = "New York" #changed "location" to "state" for greater clarity/specificity
+MAX_USER = 1000 #added an _ to follow snake_case & changed "LIMIT" to "USER" to clarify what the variable is referencing
+
+print (PI,user_age,user_state,MAX_USER) #added a print line to test that variable assignment works
